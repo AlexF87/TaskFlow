e061bdc07614f1cc7697405d110faadfd24d066b
Alexandr <feldman87@yandex.ru>
2025-04-02 16:23:39 +0700

    feat(core) Добавил класс Task и TaskStatus

---
+++ src/java/com/example/taskflow/model/Task.java
@@ -0,0 +1,8 @@
+package java.com.example.taskflow.model;
+
+public class Task {
+    private int id;
+    private String name;
+    private String description;
+    private TaskStatus status;
+}
---
+++ src/java/com/example/taskflow/model/TaskStatus.java
@@ -0,0 +1,7 @@
+package java.com.example.taskflow.model;
+
+public enum TaskStatus {
+    NEW,
+    IN_PROGRESS,
+    DONE;
+}
