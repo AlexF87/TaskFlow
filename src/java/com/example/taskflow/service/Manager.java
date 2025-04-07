ea3686ea5fd5604341902e9396beb1d6d570582b
Alexandr <feldman87@yandex.ru>
2025-04-07 14:51:21 +0700

    feat: Создать класс Manager и хранилище для задач

---
+++ src/java/com/example/taskflow/service/Manager.java
@@ -0,0 +1,12 @@
+package java.com.example.taskflow.service;
+
+import java.com.example.taskflow.model.Task;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Manager {
+    //Хранилище задач
+    Map<Integer, Task> tasks = new HashMap<>();
+
+    
+}
